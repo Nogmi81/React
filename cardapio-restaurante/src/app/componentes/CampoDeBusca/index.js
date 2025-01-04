4000a4c18711693b06d38257f2914388978650e5
André Tavares <140923882+Nogmi81@users.noreply.github.com>
2025-01-04 18:15:39 -0300

    Update index.js

--- cardapio-restaurante/src/app/componentes/CampoDeBusca/index.js
+++ cardapio-restaurante/src/app/componentes/CampoDeBusca/index.js
@@ -12,7 +12,7 @@ export default function CampoDeBusca({ textoBuscaDigitado, handleBusca }) {
           type="text"
           value={textoBuscaDigitado}
           onChange={(event) => handleBusca(event.target.value)}
-          placeholder="Pesquise um dos pratos do nosso cardápio"
+          placeholder="Pesquise um prato do cardápio"
           size="50"
         />
       </div>
