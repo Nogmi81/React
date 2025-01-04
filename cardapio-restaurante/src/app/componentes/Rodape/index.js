f2137ba77228cb12c70141d95359fce9dfa28320
André Tavares <140923882+Nogmi81@users.noreply.github.com>
2025-01-04 15:56:28 -0300

    Update index.js

--- cardapio-restaurante/src/app/componentes/Rodape/index.js
+++ cardapio-restaurante/src/app/componentes/Rodape/index.js
@@ -1,6 +1,6 @@
 import estilos from './Rodape.module.css';
 import Image from 'next/image';
-import QrCode from '../../../../public/QrCode.png'
+// import QrCode from '../../../../public/QrCode.png'
 import Link from "next/link";
 
 export default function Rodape(props) {
